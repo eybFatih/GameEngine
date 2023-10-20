e8e875a63a28464e1b1d611314a7fad8b5fd19df
eybFatih <eyuboglu.fatih@gmail.com>
2023-10-20 22:09:52 +0300

    Hello Game Engine commit

--- GameEngine.pro
+++ GameEngine.pro
@@ -1,3 +1,6 @@
 QT = core
 
 CONFIG += c++17
+
+SOURCES += \
+    Main.cpp
---
+++ Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "Hello 2D Game Engine!" << std::endl;
+
+    return 0;
+}
